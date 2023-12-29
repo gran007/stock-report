df1b84e136168bed1372aacd611fec07ac017812
gran007 <gran007@naver.com>
2023-12-29 21:28:56 +0900

    엑셀 리포트 생성 기능 추가

--- db_manager.py
+++ db_manager.py
@@ -27,7 +27,58 @@ def insert_company_type(id, name):
         db_session.add(CompanyType(id, name))
 
 
-
-
 def commit():
     db_session.commit()
+
+
+class DailyDataReport:
+    date = ''
+    code = ''
+    name = ''
+    closing_price = 0
+    fluctuation_ratio = 0.0
+    transaction_amount = 0
+
+
+def select_date_and_transaction_amount(date, transaction_amount):
+    company_types = {}
+    for c in db_session.query(CompanyType):
+        company_types[c.id] = c.name
+
+    daily_data_date = [d[0] for d in db_session.query(DailyData)
+        .with_entities(DailyData.date).distinct()
+        .order_by(DailyData.date).filter(DailyData.date.startswith(date))]
+
+    company_codes = [c[0] for c in db_session.query(DailyData.code)
+        .with_entities(DailyData.code).distinct()
+        .filter(DailyData.date.startswith(date),
+                DailyData.transaction_amount >= transaction_amount)]
+
+    daily_data = [d for d in db_session.query(DailyData)
+        .with_entities(DailyData)
+        .order_by(DailyData.code, DailyData.date)
+        .filter(DailyData.code.in_(company_codes))]
+
+    data_set = {}
+
+    for code in company_codes:
+        if code not in data_set:
+            data_set[code] = {
+                'name': company_types[code],
+                'count': 0
+            }
+
+        for date in daily_data_date:
+            items = [f for f in filter(lambda u: u.code == code and u.date == date, daily_data)]
+            if len(items) > 0:
+                item = items[0]
+                if item.transaction_amount > transaction_amount:
+                    data_set[code]['count'] += 1
+
+                data_set[code][date] = {
+                    'closing_price': item.closing_price,
+                    'fluctuation_ratio': item.fluctuation_ratio,
+                    'is_over_transaction_amount_standard': item.transaction_amount >= transaction_amount
+                }
+
+    return daily_data_date, sorted(data_set.items(), key=lambda s: s[0])
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import openpyxl
 import os
 import glob
-from db_manager import *
 
-path = 'C:\\Users\\gran007\\Desktop\\주식\\202311'
+from openpyxl.styles import Alignment, Font, PatternFill, Border, Side
+
+from db_manager import *
 
 
 def insert_excel_data():
     company_info = {}
 
-    for file_path in glob.glob(f'{path}/*.xlsx'):
+    input_path = 'C:\\Users\\gran007\\Desktop\\주식\\202311'
+    for file_path in glob.glob(f'{input_path}/*.xlsx'):
         basename = os.path.basename(file_path)
         date = os.path.splitext(basename)[0]
         wb = openpyxl.load_workbook(file_path)
@@ -26,5 +28,71 @@ def insert_excel_data():
     commit()
 
 
+headers = ['종목코드', '종목명', '거래량초과']
+
+
+def export_excel(dates, result):
+    output_path = 'C:\\Users\\gran007\\Desktop\\주식\\12월 종합.xlsx'
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.column_dimensions['A'].width = 8
+    ws.column_dimensions['B'].width = 17
+    ws.column_dimensions['C'].width = 10
+
+    thin_border = Border(left=Side(style='thin'),
+                         right=Side(style='thin'),
+                         top=Side(style='thin'),
+                         bottom=Side(style='thin'))
+    align_center = Alignment(vertical='center', horizontal='center')
+    align_right = Alignment(vertical='center', horizontal='right', wrapText=True)
+    header_bg_color = PatternFill(start_color='DBE5F1', end_color='DBE5F1', fill_type='solid')
+
+    for index, name in enumerate(headers):
+        cell = ws.cell(1, index + 1)
+        cell.value = name
+        cell.fill = header_bg_color
+        cell.alignment = align_center
+        cell.border = thin_border
+    for index, date in enumerate(dates):
+        cell = ws.cell(1, len(headers) + index + 1)
+        cell.value = date.strftime('%m월 %d일')
+        cell.fill = header_bg_color
+        ws.column_dimensions[chr(68+index)].width = 12
+        cell.alignment = align_center
+        cell.border = thin_border
+
+    for row_index, (code, item) in enumerate(result):
+        ws.cell(row_index + 2, 1).value = code
+        ws.cell(row_index + 2, 1).font = Font(bold=True)
+        ws.cell(row_index + 2, 1).alignment = align_center
+        ws.cell(row_index + 2, 2).value = item['name']
+        ws.cell(row_index + 2, 2).alignment = align_center
+        ws.cell(row_index + 2, 3).value = f"{item['count']}회"
+        ws.cell(row_index + 2, 3).alignment = align_right
+        ws.row_dimensions[row_index + 2].height = 52
+
+        for col_index, date in enumerate(dates):
+            if date in item:
+                row_item = item[date]
+                fluctuation_ratio = round(row_item['fluctuation_ratio'], 2)
+                closing_price = format(row_item['closing_price'], ',')
+                is_over_transaction_amount_standard = row_item['is_over_transaction_amount_standard']
+                cell = ws.cell(row_index + 2, col_index + 4)
+                cell.alignment = align_right
+                cell.value = f'="{fluctuation_ratio}%"&CHAR(10)&"({closing_price}원)"'
+                cell.border = thin_border
+                if fluctuation_ratio >= 0:
+                    bg_color = 'E6B9B8' if is_over_transaction_amount_standard else 'F2DDDC'
+                    cell.fill = PatternFill(start_color=bg_color, end_color=bg_color, fill_type='solid')
+                    cell.font = Font(color='FD0012', bold=True if is_over_transaction_amount_standard else False)
+                else:
+                    bg_color = '4F81BD' if is_over_transaction_amount_standard else 'B8CCE4'
+                    cell.fill = PatternFill(start_color=bg_color, end_color=bg_color, fill_type='solid')
+                    cell.font = Font(color='38497D', bold=True if is_over_transaction_amount_standard else False)
+    wb.save(output_path)
+
+
 if __name__ == '__main__':
-    insert_excel_data()
+    dates, result = select_date_and_transaction_amount('2023-12', 200_000_000_000)
+    export_excel(dates, result)
+
